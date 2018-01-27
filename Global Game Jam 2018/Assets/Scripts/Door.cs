53df21520466b268c706aa4eb0e371d78eec32f4
Kent Weiler <theswordbattler@gmail.com>
2018-01-26 22:41:08 -0600

    Added Frequency Ranges and a couple folders

---
+++ Global Game Jam 2018/Assets/Scripts/Door.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public FrequencyRange frequencyRange;
+
+	public float currentFrequency;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnEnable () {
+		EventManager.SendFrequencyEvent += SetFrequency;
+	}
+
+	void OnDisable () {
+		EventManager.SendFrequencyEvent -= SetFrequency;
+	}
+
+	public void Activate (float frequency) {
+		
+	}
+
+	public void SetFrequency(float frequency) {
+		
+	}
+}
---
+++ Global Game Jam 2018/Assets/Scripts/FrequencyRange.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "FreqRange", menuName = "Frequency Range", order = 1)]
+public class FrequencyRange : ScriptableObject {
+
+	public int min;
+	public int max;
+}
