a3b5080f7148ad8004845f08e3a9c60f3a344dbe
Kent Weiler <theswordbattler@gmail.com>
2018-01-27 11:29:10 -0600

    Stuff works.

--- Global Game Jam 2018/Assets/Scripts/PlayerController.cs
+++ Global Game Jam 2018/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour {
 
 	Rigidbody rbody;
 
+	public float maxSpeed;
+
+	public FrequencyRange freqRange;
+
+	public float currentFrequency = 1;
+
 	// Use this for initialization
 	void Start () {
 		rbody = GetComponent<Rigidbody>();
@@ -13,6 +19,51 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		rbody.velocity = new Vector3(transform.forward.x, rbody.velocity.y, transform.forward.z);
+		float xSpeed = 0;
+		float zSpeed = 0;
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
+
+		#region PlayerMovement
+		if(yAxis > 0) {
+			if(xAxis > 0.5f) {
+				xSpeed = maxSpeed;
+			} else if(xAxis < -0.5f) {
+				zSpeed = maxSpeed;
+			} else {
+				xSpeed = maxSpeed/1.5f;
+				zSpeed = maxSpeed/1.5f;
+			}
+		} else if(yAxis < -0) {
+			if(xAxis > 0.5f) {
+				zSpeed = -maxSpeed;
+			} else if(xAxis < -0.5f) {
+				xSpeed = -maxSpeed;
+			} else {
+				xSpeed = -maxSpeed/1.5f;
+				zSpeed = -maxSpeed/1.5f;
+			}
+		} else if(Mathf.Approximately(yAxis, 0)) {
+			if(xAxis > 0.5f) {
+				xSpeed = maxSpeed/1.5f;
+				zSpeed = -maxSpeed/1.5f;
+			} else if(xAxis < -0.5f) {
+				xSpeed = -maxSpeed/1.5f;
+				zSpeed = maxSpeed/1.5f;
+			}
+		}
+
+		rbody.velocity = new Vector3(xSpeed, rbody.velocity.y, zSpeed);
+		#endregion
+
+		#region FrequencyControls
+		if(Input.GetButtonDown("UpFrequency") && currentFrequency < freqRange.max) {
+			currentFrequency += 1;
+			EventManager.SendFrequency(currentFrequency);
+		} else if(Input.GetButtonDown("DownFrequency") && currentFrequency > freqRange.min) {
+			currentFrequency -= 1;
+			EventManager.SendFrequency(currentFrequency);
+		}
+		#endregion
 	}
 }
