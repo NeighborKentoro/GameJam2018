a6157495286564aed363bb37410a247c8e3e656a
Kent Weiler <theswordbattler@gmail.com>
2018-01-26 22:14:41 -0600

    Added event manager and ActivableObject script. Also added my test scene

---
+++ Global Game Jam 2018/Assets/Scripts/EventManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+	public delegate void SendFrequencyAction(float frequency);
+
+	public static event SendFrequencyAction SendFrequencyEvent;
+
+
+	public static void SendFrequency(float frequency) {
+		SendFrequencyEvent(frequency);
+	}
+}
